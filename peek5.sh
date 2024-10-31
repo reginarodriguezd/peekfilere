08243f3657337a1275b21201fa7a9ca458010274
Regina <reginarodriguezdurant@live.com.mx>
2024-10-31 12:21:48 +0100

    fourth commit

--- peek5.sh
+++ peek5.sh
@@ -5,6 +5,18 @@ if [[ -n $2 ]]; then
 LINES=$2;
 else
 LINES=3;
+fi;
+
+TOTLINES=$(wc -l < "$FILE")
+
+if ((TOTLINES <= 2 * LINES)); then 
+	cat "$FILE"
+else
+	echo "Warning message: The file contains more than ${LINES} last 
+lines."
+	head -n "$LINES" "$FILE"
+	echo "..."
+	tail -n "$LINES" "$FILE"
 fi
 
 (head -n $LINES $FILE  && tail -n $LINES $FILE)
